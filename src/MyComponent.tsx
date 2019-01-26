993295300cdcb11bd1dbcdae84e114361daeaa10
David Gruar <davidgruar@gmail.com>
2019-01-26 22:18:02 +0000

    Update readme

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { getMessage } from "./getMessage";
 
 export const MyComponent: React.SFC<{foo: number}> = ({foo}) => {
     return (
         <div>
-            The number is {foo}.
+            {getMessage(foo)}
         </div>
     )
 }
